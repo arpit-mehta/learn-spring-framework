ebf79d02d30a29cccdcbf7eca67e495566624da2
arpit-mehta <iarpit.mehta@gmail.com>
2023-01-05 14:42:41 +0530

    Beans created

---
+++ learn-spring-framework/src/main/java/com/myCode/learnspringframework/FirstSpring/App02HelloWorldSpring.java
@@ -0,0 +1,20 @@
+package com.myCode.learnspringframework.FirstSpring;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App02HelloWorldSpring {
+    public static void main(String[] args) {
+
+        //1) Launch Spring Context
+        var context = new AnnotationConfigApplicationContext (HelloWorldConfiguration.class);
+
+        //2) Configure the things we want Spring to manage
+        //HelloWordConfiguration -> @Configuration
+
+        //3) access Beans manages by spring
+        System.out.println(context.getBean("name"));
+        System.out.println(context.getBean("age"));
+        System.out.println(context.getBean("person"));
+        System.out.println(context.getBean("CustomBeanName"));
+    }
+}
---
+++ learn-spring-framework/src/main/java/com/myCode/learnspringframework/FirstSpring/HelloWorldConfiguration.java
@@ -0,0 +1,37 @@
+package com.myCode.learnspringframework.FirstSpring;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+record Person (String name,int age){ };
+record Address (String firstLine,String city){ };
+
+//in here we define spring beans
+//in the configuration class we define methods to define beans
+@Configuration
+public class HelloWorldConfiguration {
+
+    @Bean
+    public String name(){
+        return "Arpit Mehta";
+    }
+
+    @Bean
+    public int age(){
+        return 22;
+    }
+
+    @Bean
+    public Person person(){
+        return new Person("Eva",14);
+    }
+
+    @Bean(name="CustomBeanName")
+    public Address address(){
+        return new Address("Opposite Uco Bank","Dharamshala");
+    }
+
+
+
+
+}
