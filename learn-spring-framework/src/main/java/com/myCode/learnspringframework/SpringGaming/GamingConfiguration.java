3977d09773ecff1b9211f6cb91ee4e227ddfb1c6
arpit-mehta <iarpit.mehta@gmail.com>
2023-01-05 17:24:40 +0530

    Launched all Games using Spring

--- learn-spring-framework/src/main/java/com/myCode/learnspringframework/SpringGaming/GamingConfiguration.java
+++ learn-spring-framework/src/main/java/com/myCode/learnspringframework/SpringGaming/GamingConfiguration.java
@@ -5,20 +5,43 @@ import com.myCode.learnspringframework.Gaming.GamingConsole;
 import com.myCode.learnspringframework.Gaming.PacManGame;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class GamingConfiguration {
 
     @Bean
-    public GamingConsole game(){
+    public GamingConsole game1(){
         var game = new PacManGame();
-
         return game;
     }
 
     @Bean
-    public GameRunner gameRunner(GamingConsole game){
-        var gameRunner = new GameRunner(game);
+    public GamingConsole game2(){
+        var game = new MarioGame();
+        return game;
+    }
+
+    @Bean
+    public GamingConsole game3(){
+        var game = new SuperContraGame();
+        return game;
+    }
+
+    @Bean
+    @Primary
+    public GameRunner gameRunner1(GamingConsole game1){
+        var gameRunner = new GameRunner(game1);
+        return gameRunner;
+    }
+    @Bean
+    public GameRunner gameRunner2(GamingConsole game2){
+        var gameRunner = new GameRunner(game2);
+        return gameRunner;
+    }
+    @Bean
+    public GameRunner gameRunner3(GamingConsole game3){
+        var gameRunner = new GameRunner(game3);
         return gameRunner;
     }
 }
