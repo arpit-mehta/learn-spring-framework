cdcdc4ab9cea0b29b2eb9e678a024f5f7e471d9e
arpit-mehta <iarpit.mehta@gmail.com>
2023-01-06 15:39:53 +0530

    Field Injection

---
+++ learn-spring-framework-02/src/main/java/com/myCode/learnspringframework/DependencyInjection/DependencyInjectionLauncherApplication.java
@@ -0,0 +1,44 @@
+package com.myCode.learnspringframework.DependencyInjection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.*;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Component
+class HelloInjection{
+
+    @Autowired
+    Dependency1 dependency1;
+
+    @Autowired
+    Dependency2 dependency2;
+
+    public String toString(){
+        return "Using " + dependency1 +" and " + dependency2;
+    }
+
+
+}
+@Component
+class Dependency1{
+
+}
+@Component
+class Dependency2{
+
+}
+@Configuration
+@ComponentScan //performs component scan on current package
+public class DependencyInjectionLauncherApplication {
+
+    public static void main(String[] args) {
+        try (var context = new AnnotationConfigApplicationContext(DependencyInjectionLauncherApplication.class)) {
+
+            Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
+
+            System.out.println(context.getBean(HelloInjection.class));
+        }
+    }
+}
