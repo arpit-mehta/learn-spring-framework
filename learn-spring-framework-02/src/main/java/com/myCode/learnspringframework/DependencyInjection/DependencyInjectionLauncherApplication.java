000487cdb167215ad861a4bee978a06343ac62f9
arpit-mehta <iarpit.mehta@gmail.com>
2023-01-06 15:45:36 +0530

    Constructor Injection

--- learn-spring-framework-02/src/main/java/com/myCode/learnspringframework/DependencyInjection/DependencyInjectionLauncherApplication.java
+++ learn-spring-framework-02/src/main/java/com/myCode/learnspringframework/DependencyInjection/DependencyInjectionLauncherApplication.java
@@ -12,15 +12,21 @@ class HelloInjection{
     Dependency1 dependency1;
     Dependency2 dependency2;
 
-    @Autowired
-    public void setDependency1(Dependency1 dependency1) {
-        this.dependency1 = dependency1;
-    }
 
-    @Autowired
-    public void setDependency2(Dependency2 dependency2) {
+    public HelloInjection(Dependency1 dependency1, Dependency2 dependency2) {
+        this.dependency1 = dependency1;
         this.dependency2 = dependency2;
     }
+
+    //    @Autowired
+//    public void setDependency1(Dependency1 dependency1) {
+//        this.dependency1 = dependency1;
+//    }
+//
+//    @Autowired
+//    public void setDependency2(Dependency2 dependency2) {
+//        this.dependency2 = dependency2;
+//    }
 
     public String toString(){
         return "Using " + dependency1 +" and " + dependency2;
